85aeeda004e02174fb643a86355f3e3cfc311572
void-the-droid <bhuwan626282@gmail.com>
2024-04-26 21:49:51 +0545

    kuch samajha nahi aya

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
 
-const title =document.getElementById("title")
-const author = document.getElementById("author")
+const titleField =document.getElementById("titleField")
+const authorField = document.getElementById("authorField")
 const AddBtn = document.getElementById("add-btn")
 const Container = document.getElementById("container")
 const  Div_Form = document.getElementById("form")
 const AddSection = document.getElementById("AddBook")
 const Main_Div = document.getElementById("main")
-
+const Addbtn =document.getElementById("add-btn")
+const item =document.getElementsByClassName("item")
 
 
 
@@ -30,9 +31,34 @@ AddSection.addEventListener('click',()=>{
         Div_Form.classList.add("input-container");
         Container.style.opacity=1;
 
+        
         //Giving the newly created div class name
         for( let i=1; i<=1000; i++){
-        children= Main_Div.children[i]
-        children.classList.add("item")}
-    })
+            AddbookHandle()
+      const children= Main_Div.children[i]
+        children.classList.add("item")
+        children.id=`item${i}`
+        item.innerHTML= "title : " + titleValue + "\nAuthor: "+authorValue;
 
+    }
+
+  
+      
+    })
+    const output = document.getElementById('output');
+
+
+    //Adding book-data to document
+     
+    function AddbookHandle(){
+       
+        const titleValue = titleField.value
+        const authorValue = authorField.value
+     }
+
+    
+
+
+
+
+  
