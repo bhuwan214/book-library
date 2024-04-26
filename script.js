24507d0cce91bfb09d64d12eb573bcfd99cec914
void-the-droid <bhuwan626282@gmail.com>
2024-04-26 08:13:57 +0545

    creating div part is done

--- script.js
+++ script.js
@@ -5,16 +5,34 @@ const AddBtn = document.getElementById("add-btn")
 const Container = document.getElementById("container")
 const  Div_Form = document.getElementById("form")
 const AddSection = document.getElementById("AddBook")
+const Main_Div = document.getElementById("main")
+
+
+
 
 
 //Getting the Input filed and blurring other content on the site
 AddSection.addEventListener('click',()=>{
-
-    Container.style.zIndex=0;
     Container.style.opacity=.4;
     Div_Form.classList.remove("input-container")
 
-
-
-
 })
+
+
+
+
+    AddBtn.addEventListener( "click", () => {
+        
+        //Creating a div inside the main-div
+        const newDiv=document.createElement("div")
+        Main_Div.appendChild(newDiv)
+    
+        Div_Form.classList.add("input-container");
+        Container.style.opacity=1;
+
+        //Giving the newly created div class name
+        for( let i=1; i<=1000; i++){
+        children= Main_Div.children[i]
+        children.classList.add("item")}
+    })
+
